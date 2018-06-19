b7d0ec0e2a6524e0b27ce0173c57ba893784fbde
theguyfromtv <goldfingerorama@gmail.com>
2018-06-19 15:58:29 -0400

    accordion gallery prototype

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 jQuery(document).ready(function () {
-    $('#block-1').on('click mouseover', function () {
-        $(this).finish().addClass('info-block-expanded');
-        $('#block-2').finish().removeClass('info-block-expanded info-block-blue').addClass('info-block-orange');
-        $('#block-3').finish().removeClass('info-block-expanded info-block-gray').addClass('info-block-blue');
-        $('#block-4').finish().removeClass('info-block-expanded').addClass('info-block-gray');
+    $('.element').on('click', function () {
+        console.log("click")
+        $(this).addClass('elementExpanded');
+        $(this).animate({width:"350px"});
+        $('.element').not(this).removeClass('elementExpanded')
     });
-    $('#block-2').on('click mouseover', function () {
+    $('#block-2').on('click', function () {
         $(this).finish().addClass('info-block-expanded');
         $('#block-1').finish().removeClass('info-block-expanded').addClass('info-block-orange');
         $('#block-3').finish().removeClass('info-block-expanded info-block-gray').addClass('info-block-blue');
         $('#block-4').finish().removeClass('info-block-expanded').addClass('info-block-gray');
     });
-    $('#block-3').on('click mouseover', function () {
+    $('#block-3').on('click', function () {
         $(this).finish().addClass('info-block-expanded');
         $('#block-1').finish().removeClass('info-block-expanded').addClass('info-block-orange');
         $('#block-2').finish().removeClass('info-block-expanded info-block-orange').addClass('info-block-blue');
         $('#block-4').finish().removeClass('info-block-expanded').addClass('info-block-gray');
     });
-    $('#block-4').on('click mouseover', function () {
+    $('#block-4').on('click', function () {
         $(this).finish().addClass('info-block-expanded');
         $('#block-1').finish().removeClass('info-block-expanded').addClass('info-block-orange');
         $('#block-2').finish().removeClass('info-block-expanded info-block-orange').addClass('info-block-blue');
